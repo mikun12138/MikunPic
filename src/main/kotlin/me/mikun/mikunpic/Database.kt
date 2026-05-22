3698fc6d78196dcc8328cfb9156ab567576e1345
mikun <mikun.12138@gmail.com>
2026-05-22 11:17:24 +0800

    clean env

--- src/main/kotlin/me/mikun/mikunpic/Database.kt
+++
@@ -1,8 +0,0 @@
-package me.mikun.mikunpic
-
-import io.ktor.server.application.Application
-import org.jetbrains.exposed.v1.jdbc.Database
-
-fun Application.configureDatabase() {
-    Database.connect("jdbc:sqlite:./data/databases/pic.db", driver = "org.sqlite.JDBC")
-}
--- src/test/kotlin/sandbox.kt
+++
@@ -1,3 +0,0 @@
-package me.mikun
-
-val downloadFolder = "test/d/"
